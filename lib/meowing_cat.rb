93990d9f0bca7efaddd2132940cc652aefcd28c7
Mary Selig <seligmar@gmail.com>
2019-07-17 11:01:35 +0000

    Automatically backed up by Learn

--- lib/meowing_cat.rb
+++ lib/meowing_cat.rb
@@ -2,12 +2,11 @@
 class Cat 
   attr_accessor :name 
   
-  def meow=(meow)
-    @meow = meow
-  end 
+  # def meow=(meow)
+  #   @meow = meow
+  # end 
   
   def meow
-   # @meow
     puts "meow"
   end 
   
